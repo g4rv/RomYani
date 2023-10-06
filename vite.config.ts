a57d36c0fbd20571f3eeac283b597a3bd3f1317c
g4rv <skydorw@gmail.com>
2023-10-06 18:43:43 +0300

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+    base: '',
 	build: {
 		outDir: "./docs",
+        rollupOptions: {
+            
+            output: {
+                dir: "./docs",
+                
+            }
+        }
 	},
 });

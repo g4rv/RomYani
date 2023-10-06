79ffab20e7f60930d6a6dc5502d72353938fe256
g4rv <skydorw@gmail.com>
2023-10-06 19:53:18 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
-import Home from './pages/Home';
-import Tests from './pages/Tests';
-import Greeting from './pages/Greeting';
-import GreetingsLayout from './layouts/GreetingsLayout';
-
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import Tests from "./pages/Tests";
+import Greeting from "./pages/Greeting";
+import GreetingsLayout from "./layouts/GreetingsLayout";
 
 const App = () => {
-    return (
-        <Routes>
-			<Route path='/RomYani/*' element={<MainLayout />}>
+	return (
+		<Routes>
+			<Route path="/*" element={<MainLayout />}>
 				<Route index element={<Home />} />
 				<Route path=":bunny/test" element={<Tests />} />
 			</Route>
-            <Route path='/RomYani/:bunny/greeting' element={<GreetingsLayout />}>
+			<Route path="/:bunny/greeting" element={<GreetingsLayout />}>
 				<Route index element={<Greeting />} />
 			</Route>
 		</Routes>
-    );
+	);
 };
 export default App;

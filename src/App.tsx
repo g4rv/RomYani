5a583cc8907abb4584eeda236c823b54ac1229f9
g4rv <skydorw@gmail.com>
2023-10-06 18:48:51 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ import GreetingsLayout from './layouts/GreetingsLayout';
 const App = () => {
     return (
         <Routes>
-			<Route path='https://g4rv.github.io/RomYani/*' element={<MainLayout />}>
+			<Route path='g4rv.github.io/RomYani/*' element={<MainLayout />}>
 				<Route index element={<Home />} />
-				<Route path="https://g4rv.github.io/RomYani/:bunny/test" element={<Tests />} />
+				<Route path="g4rv.github.io/RomYani/:bunny/test" element={<Tests />} />
 			</Route>
-            <Route path='https://g4rv.github.io/RomYani/:bunny/greeting' element={<GreetingsLayout />}>
+            <Route path='g4rv.github.io/RomYani/:bunny/greeting' element={<GreetingsLayout />}>
 				<Route index element={<Greeting />} />
 			</Route>
 		</Routes>

0902b1a7f6e96c696dcc71a9db02512f54044050
g4rv <skydorw@gmail.com>
2023-10-06 18:46:49 +0300

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ import GreetingsLayout from './layouts/GreetingsLayout';
 const App = () => {
     return (
         <Routes>
-			<Route path='/*' element={<MainLayout />}>
+			<Route path='https://g4rv.github.io/RomYani/*' element={<MainLayout />}>
 				<Route index element={<Home />} />
-				<Route path=":bunny/test" element={<Tests />} />
+				<Route path="https://g4rv.github.io/RomYani/:bunny/test" element={<Tests />} />
 			</Route>
-            <Route path=':bunny/greeting' element={<GreetingsLayout />}>
+            <Route path='https://g4rv.github.io/RomYani/:bunny/greeting' element={<GreetingsLayout />}>
 				<Route index element={<Greeting />} />
 			</Route>
 		</Routes>

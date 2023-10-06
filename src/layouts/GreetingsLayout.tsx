c76d13053523f3a3a91947134f38b88d789011d2
g4rv <skydorw@gmail.com>
2023-10-06 22:27:56 +0300

    fixes

--- src/layouts/GreetingsLayout.tsx
+++ src/layouts/GreetingsLayout.tsx
@@ -1,18 +1,23 @@
 import { Outlet } from "react-router-dom";
 
 const GreetingsLayout = () => {
-    return (
-        <main className="min-h-[100dvh] bg-main bg-cover relative isolate">
-            <div className="-z-10 absolute inset-0 bg-orange-800/5 backdrop-blur-sm"/>
-            <section>
-                <Outlet/>
-                <p>Я так і не вигадав куди це приліпить, тому просто вставлю як є...</p>
-                <div>
-                    <p>Займіться!</p>
-                    <p>69</p>
-                </div>
-            </section>
-        </main>
-    );
+	return (
+		<main className="relative isolate min-h-[100dvh] bg-main bg-cover">
+			<div className="absolute inset-0 -z-10 bg-orange-800/5 backdrop-blur-sm" />
+			<section className="flex flex-col gap-16 p-6">
+				<Outlet />
+				<div className="rounded-xl border border-black/10 bg-gray-500/10 px-4 py-6 backdrop-blur-xl">
+					<p>
+						Я так і не вигадав куди це приліпить, тому просто
+						вставлю як є...
+					</p>
+					<div>
+						<p>Займіться!</p>
+						<p>69</p>
+					</div>
+				</div>
+			</section>
+		</main>
+	);
 };
 export default GreetingsLayout;
